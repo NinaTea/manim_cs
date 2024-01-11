780de9313be664d0d8782e3b51dd1e255b5420ed
NIna <camilagallo1700@gmail.com>
2024-01-11 01:23:52 -0300

    First try of binaryTry in manim community edition

---
+++ scene.py
@@ -0,0 +1,46 @@
+from manim import *
+
+class BinaryTree(Scene):
+    def construct(self):
+        tree = self.create_binary_tree()
+        self.play(Create(tree))
+
+    def create_binary_tree(self):
+        nodes = [TreeNode(str(i)) for i in range(7)]
+
+        # Set positions for each node
+        nodes[0].shift(2*UP)
+
+        nodes[1].shift(2*LEFT)
+        nodes[3].shift(4 * LEFT + 2*DOWN)
+        nodes[4].shift(2*DOWN)
+
+
+        nodes[2].shift(2*RIGHT)
+        nodes[5].shift(2*DOWN)
+        nodes[6].shift(4*RIGHT+2*DOWN)
+
+        # Connect nodes to create edges
+        edges = [
+            (0, 1),
+            (0, 2),
+            (1, 3),
+            (1, 4),
+            (2, 5),
+            (2, 6)
+        ]
+
+        # Create tree
+        tree = VGroup(*nodes)
+        for edge in edges:
+            tree.add(self.connect_nodes(nodes[edge[0]], nodes[edge[1]]))
+
+        return tree
+
+    def connect_nodes(self, node1, node2):
+        return Line(node1.get_center(), node2.get_center(), buff=0.1)
+
+class TreeNode(Tex):
+    def __init__(self, label, **kwargs):
+        super().__init__(label,**kwargs)
+
